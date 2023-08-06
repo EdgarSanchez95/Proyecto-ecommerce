84ca8ed4de5269890641dc7690daeb7eab92b0da
Edgar Moises Sanchez Reyes <edgar_95_san@hotmail.com>
2023-08-06 00:19:24 -0700

    Se crean la parte de diseño y vistas

---
+++ src/main/java/com/curso/ecommerce/controller/AdministradorController.java
@@ -0,0 +1,21 @@
+
+package com.curso.ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author edgar
+ */
+@Controller
+@RequestMapping("/administrador")
+public class AdministradorController {
+    
+    @GetMapping("")
+    public String home(){
+    
+        return "administrador/home";
+    }
+}
