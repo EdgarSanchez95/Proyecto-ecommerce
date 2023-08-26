5ba62efce999f5b1746f7460e2a1b8b911470881
Edgar Moises Sanchez Reyes <edgar_95_san@hotmail.com>
2023-08-26 00:10:17 -0700

    se crea el template del usuario y la funcionalidad para mostrarlos datos del producto en otra vista

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -2,10 +2,13 @@
 package com.curso.ecommerce.controller;
 
 import com.curso.ecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -16,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
     
+    private final Logger log= LoggerFactory.getLogger(HomeController.class);
+    
     @Autowired
     private ProductoService productoService;
     
@@ -24,5 +29,12 @@ public class HomeController {
     
         model.addAttribute("productos", productoService.findAll());
         return "usuario/home";
+    }
+    
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer id){
+    
+        log.info("Id producto enviado como parametro {}", id);
+        return "usuario/productohome";
     }
 }
