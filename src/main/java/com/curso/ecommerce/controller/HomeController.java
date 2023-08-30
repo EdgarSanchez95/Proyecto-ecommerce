81ad9b1c8b9d9d925464e694c8fd9a18abc7e602
Edgar Moises Sanchez Reyes <edgar_95_san@hotmail.com>
2023-08-29 22:21:27 -0700

    se crea la funcionalidad para redireccionar a la vista carrito

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -43,5 +44,11 @@ public class HomeController {
         
         model.addAttribute("producto", producto);
         return "usuario/productohome";
+    }
+    
+    @PostMapping("/cart")
+    public String  addCart(){
+    
+        return "usuario/carrito";
     }
 }
