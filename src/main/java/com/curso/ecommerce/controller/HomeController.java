fe1b4df6f1fbdbeb102600512744b20b1df8caca
Edgar Moises Sanchez Reyes <edgar_95_san@hotmail.com>
2023-09-01 23:57:43 -0700

    Funcionalidad eliminar productos del carrito

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -84,4 +84,31 @@ public class HomeController {
         model.addAttribute("orden", orden);
         return "usuario/carrito";
     }
+
+    //quitar un producto del carrito
+    @GetMapping("/delete/cart/{id}")
+    public String deleteProductoCart(@PathVariable Integer id, Model model){
+
+        //lista nueva de productos
+        List<DetalleOrden> ordenesNueva = new ArrayList<DetalleOrden>();
+
+        for(DetalleOrden detalleOrden: detalles){
+            if(detalleOrden.getProducto().getId()!=id){
+
+                ordenesNueva.add(detalleOrden);
+            }
+
+        }
+
+        detalles = ordenesNueva;
+
+        double sumaTotal = 0;
+        sumaTotal = detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
+
+        orden.setTotal(sumaTotal);
+        model.addAttribute("cart", detalles);
+        model.addAttribute("orden", orden);
+
+        return "usuario/carrito";
+    }
 }
